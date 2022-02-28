cdf0f6ecdb20adac91e4b8987606843cb630e1ac
BiAniTy <drayttc@gmail.com>
2022-02-28 18:58:35 -0300

    Complete js-exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+    switch (true) {
+        case num < 0:
+            return "ERROR";
+    }
 
+    let text = "";
+
+    for (let i = 0; i < num; i++) {
+        text = text.concat(string);
+    }
+
+    return text;
 };
 
 // Do not edit below this line
