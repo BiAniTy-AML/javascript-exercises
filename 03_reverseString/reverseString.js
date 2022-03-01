b7668d0af3e1103923c082b593dc63d4c26ee45e
BiAniTy <drayttc@gmail.com>
2022-03-01 08:04:24 -0300

    Complete js-execises 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function (string) {
+    let text = "";
 
+    for (let i = 0; i < string.length; i++) {
+        text = text.concat(string.substr(-i - 1, 1));
+    }
+
+    return text;
 };
 
 // Do not edit below this line
