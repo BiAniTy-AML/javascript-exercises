f9a6983a3efb1ca64a18cdd2f6c7f39881ba59a8
BiAniTy <drayttc@gmail.com>
2022-03-01 10:06:54 -0300

    Complete js-exercises 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,23 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    switch (true) {
+        case year % 100 === 0:
+            switch (true) {
+                case year % 400 === 0:
+                    return true;
 
+                default:
+                    return false;
+            }
+
+        case year % 4 === 0:
+            return true;
+
+        case year % 4 !== 0:
+            return false;
+
+        default:
+            return "Error with the leapYears() function";
+    }
 };
 
 // Do not edit below this line
