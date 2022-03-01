3fd332c6e053e8130e6ea82feec32189cc268e95
BiAniTy <drayttc@gmail.com>
2022-03-01 10:26:19 -0300

    Complete js-exxercises 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function (f) {
+    return (((f - 32) * (5 / 9)).toFixed(1) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (c) {
+    return ((c * (9 / 5) + 32).toFixed(1) * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+    ftoc,
+    ctof,
 };
