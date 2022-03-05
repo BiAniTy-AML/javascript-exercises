8ff614d3ece483ddea7470661efa1593a5c07a18
BiAniTy <drayttc@gmail.com>
2022-03-05 10:44:10 -0300

    Complete js-exercises 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (position) {
+    switch (true) {
+        case position < 0:
+            return "OOPS";
+    }
+    let nacci_seq = [];
 
+    for (let i = 0; i < position; i++) {
+        nacci_seq.length === 0 || nacci_seq.length === 1
+            ? nacci_seq.push(1)
+            : nacci_seq.push(nacci_seq[i - 1] + nacci_seq[i - 2]);
+    }
+
+    return nacci_seq[position - 1];
 };
 
 // Do not edit below this line
