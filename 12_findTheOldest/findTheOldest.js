3cfc60e85c1f0c9457f2ca08fda20738febd501f
BiAniTy <drayttc@gmail.com>
2022-03-05 14:23:27 -0300

    Complete js-exercises 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+    let highest_age = people.reduce((oldest_age, person) => {
+        !person.yearOfDeath
+            ? (person.yearOfDeath = new Date().getFullYear())
+            : (person = person);
 
+        return person.yearOfDeath - person.yearOfBirth > oldest_age
+            ? (oldest_age = person.yearOfDeath - person.yearOfBirth)
+            : (oldest_age = oldest_age);
+    }, 0);
+
+    let oldest = people.filter(
+        (person) => person.yearOfDeath - person.yearOfBirth === highest_age
+    );
+
+    return oldest[0];
 };
 
 // Do not edit below this line
