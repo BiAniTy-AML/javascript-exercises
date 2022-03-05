79ac97f3e1bde81a1aa68fdc9c7e8a9a6d58c6bc
BiAniTy <drayttc@gmail.com>
2022-03-05 09:52:03 -0300

    Complete js-execises 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,34 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    function is_letter(c) {
+        return c.toLowerCase() !== c.toUpperCase();
+    }
 
+    let only_letters = "";
+    for (let i = 0; i < string.length; i++) {
+        if (is_letter(string.charAt(i))) {
+            only_letters += string.charAt(i);
+        }
+    }
+
+    only_letters = only_letters.toUpperCase();
+
+    for (let i = 0; i < only_letters.length; i++) {
+        let base = only_letters.charAt(i);
+        let mirror = only_letters.charAt(only_letters.length - i - 1);
+
+        switch (base === mirror) {
+            case true:
+                break;
+
+            case false:
+                return false;
+
+            default:
+                return "Problem with palindromes() func";
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
